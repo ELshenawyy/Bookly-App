cbcf1e660505adf3317e182228346a53e6b3ab01
ELshenawyy <alshnawyahmd668@gmail.com>
2024-02-03 01:27:54 +0200

    search view body done

--- lib/Features/home/presentation/views/custom_best_seller_view_item.dart
+++ lib/Features/home/presentation/views/custom_best_seller_view_item.dart
@@ -6,8 +6,8 @@ import 'package:bookly_app/core/ulits/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
+class BookListViewItem extends StatelessWidget {
+  const BookListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/Features/search/presentation/view/widget/search_view_body.dart
+++ lib/Features/search/presentation/view/widget/search_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/ulits/styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../home/presentation/views/custom_best_seller_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -10,9 +12,42 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "Search Result",
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          BookSearchListView(),
         ],
+      ),
+    );
+  }
+}
+
+class BookSearchListView extends StatelessWidget {
+  const BookSearchListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: BookListViewItem(),
+          );
+        },
+        itemCount: 10,
       ),
     );
   }
